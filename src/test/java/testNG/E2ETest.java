73147caf7df2685015a7dd938ad0de94170dfb2c
RameshTulal <rameshtulal@yahoo.com>
2020-10-04 21:01:00 +0200

    commit 3

--- src/test/java/testNG/E2ETest.java
+++ src/test/java/testNG/E2ETest.java
@@ -1,40 +1,20 @@
-package testNGTest;
+package testNG;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class TestNG_Test {
+public class E2ETest {
+
 	WebDriver driver = null;
 	static String baseUrl = "https://www.seleniumeasy.com/test/charts-mouse-hover-demo.html"; 		
-	
-	@BeforeSuite
-	public void setupSuit() {
-		System.out.println("Suit Setup Started");
-	}
-	
-	@BeforeTest
-	public void setupTests() {
-		System.out.println("Tests Setup Started");
-	}
-	
-	@BeforeClass
-	public void setupClass() {
-		System.out.println("Class Setup Started");
-	}
 	
 	@BeforeMethod
 	public void setupMethod() throws InterruptedException {
@@ -53,14 +33,6 @@ public class TestNG_Test {
 		System.out.println("Test case 1 Executed");
 	}
 	
-	@Test
-	public void TestCase2() throws InterruptedException {
-		driver.get(baseUrl);
-		Thread.sleep(2000);
-		driver.manage().window().maximize();
-		Assert.assertEquals("String","String", "Test Failed: No element found");
-		System.out.println("Test case 2 Executed");
-	}
 	
 	@AfterMethod
 	public void WindUpMethodSetUp() throws InterruptedException {	
@@ -68,21 +40,5 @@ public class TestNG_Test {
 		driver.close();
 		System.out.println("Method Setup Finished");
 	}
-	
-	@AfterClass
-	public void WindUpClassSetUp() {
-		System.out.println("Class Setup Finished");
-	}
-	
-	@AfterTest
-	public void WindUpTestsSetUp() {
-		System.out.println("Tests Setup Finished");
-	}
-	
-	@AfterSuite
-	public void WindUpSuitSetUp() {
-		System.out.println("Suit Setup Finished");
-	}	
-	
 
 }
